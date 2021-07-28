6aabb7230e919c22cddb696bdbf95dfb4e31bbd0
orijoon98 <orijoon98@gmail.com>
2021-07-29 01:40:40 +0900

    2021.07.30

--- C++/가장큰정사각형_1915.cpp
+++ C++/가장큰정사각형_1915.cpp
@@ -5,40 +5,40 @@ int n, m, ans;
 int board[1002][1002];
 
 void Input() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		string str;
-		cin >> str;
-		for (int j = 0; j < m; j++) {
-			board[i][j] = str[j] - 48;
-		}
-	}
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+        for (int j = 0; j < m; j++) {
+            board[i][j] = str[j] - 48;
+        }
+    }
 }
 
 void Sol() {
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j < m; j++) {
-			if (board[i][j] == 0) continue;
-			board[i][j] = min(board[i - 1][j - 1], min(board[i - 1][j], board[i][j - 1])) + 1;
-			ans = max(ans, board[i][j]);
-		}
-	}
-	cout << ans * ans << '\n';
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (board[i][j] == 0) continue;
+            board[i][j] = min(board[i - 1][j - 1], min(board[i - 1][j], board[i][j - 1])) + 1;
+            ans = max(ans, board[i][j]);
+        }
+    }
+    cout << ans * ans << '\n';
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	Input();
-	if (n < 2 || m < 2) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (board[i][j] == 1) {
-					cout << 1 << '\n';
-					return 0;
-				}
-			}
-		}
-	}
-	Sol();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Input();
+    if (n < 2 || m < 2) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1) {
+                    cout << 1 << '\n';
+                    return 0;
+                }
+            }
+        }
+    }
+    Sol();
 }

947bbf68082ddc0d34c13e069b308a71a735d24a
HyeokJoon Kong <orijoon98@gmail.com>
2022-05-02 10:56:47 +0900

    [ADD] 1931

--- C++/baekjoon/회의실배정_1931.cpp
+++ C++/baekjoon/회의실배정_1931.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+
 using namespace std;
 
 int N;
 vector<pair<int, int>> v, ans;
 
-
-void Input(){
+void Input() {
     cin>>N;
-    for(int i=0;i<N;i++){
-        int start, finish;
-        cin>>start>>finish;
-        v.push_back({finish,start});
+    for (int i = 0; i < N; i++) {
+        int start, end;
+        cin>>start>>end;
+        v.push_back({end, start});
     }
 }
 
-void Solution(){
-    sort(v.begin(), v.end()); // 도착시간 순으로 정렬
+void Solution() {
+    sort(v.begin(), v.end()); // 끝나는 시간 순으로 정렬
     ans.push_back(v[0]);
-    for(int i=1;i<v.size();i++){
-        int arrive = ans[ans.size()-1].first; // 마지막 도착시간
-        if(v[i].second >= arrive) ans.push_back(v[i]);
+    for (int i = 1; i < v.size(); i++) {
+        int end = ans[ans.size()-1].first; // 마지막 회의가 끝나는 시간
+        if (v[i].second >= end) ans.push_back(v[i]);
     }
     cout<<ans.size()<<endl;
 }
 
-void Solve(){
+void Solve() {
     Input();
     Solution();
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);

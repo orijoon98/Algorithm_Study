1250bf3b91b59e3460f063adb16388836274ef0b
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-29 15:48:24 +0900

    [ADD] test.cpp

---
+++ C++/test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define MAX INT_MAX
+
+using namespace std;
+
+void Input() {
+
+}
+
+void Solution() {
+    
+}
+
+void Solve() {
+    Input();
+    Solution();
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    Solve();
+
+    return 0;
+}

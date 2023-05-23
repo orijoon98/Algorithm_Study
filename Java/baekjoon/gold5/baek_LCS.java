2a20c2fa329ce63be65d167591960fadc52b2bc9
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-23 14:20:35 +0900

    [ADD] baekjoon gold5 LCS

---
+++ Java/baekjoon/gold5/baek_LCS.java
@@ -0,0 +1,41 @@
+package baekjoon.gold5;
+
+import java.io.*;
+
+public class baek_LCS {
+
+    static char[] A, B;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+        output();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        A = br.readLine().toCharArray();
+        B = br.readLine().toCharArray();
+    }
+
+    static void solution() {
+        int[][] lcs = new int[A.length + 1][B.length + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                } else {
+                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+                }
+                answer = Math.max(answer, lcs[i][j]);
+            }
+        }
+    }
+
+    static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.flush();
+    }
+}

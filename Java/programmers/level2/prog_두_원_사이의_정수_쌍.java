ae829c489204221e8db9778beb820b605c368655
HyeokJoon Kong <orijoon98@gmail.com>
2023-07-06 23:52:32 +0900

    [ADD] programmers level2 두 원 사이의 정수 쌍

---
+++ Java/programmers/level2/prog_두_원_사이의_정수_쌍.java
@@ -0,0 +1,15 @@
+package programmers.level2;
+
+public class prog_두_원_사이의_정수_쌍 {
+	public long solution(int r1, int r2) {
+		long answer = 0;
+
+		for (int i = 1; i <= r2; i++) {
+			long minJ = (int)Math.ceil(Math.sqrt(1.0 * r1 * r1 - 1.0 * i * i));
+			long maxJ = (int)Math.floor(Math.sqrt(1.0 * r2 * r2 - 1.0 * i * i));
+			answer += (maxJ - minJ + 1);
+		}
+
+		return answer * 4;
+	}
+}
